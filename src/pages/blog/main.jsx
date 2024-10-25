0e2c3fa7a20e39f389d3c8d7b957c6ee230e0e75
N0xCam <167630075+N0xCam@users.noreply.github.com>
2024-10-25 20:22:45 +0200

    legal-ok

--- src/pages/blog/main.jsx
+++ src/pages/blog/main.jsx
@@ -4,13 +4,14 @@ import Card from 'react-bootstrap/Card';
 const Blog = () => {
     return (
         <div className="Blog">
-          <div id="banner-blog"></div>
+          <div id="banner"></div>
             <h2 id="title-blog"> BLOG</h2>
             <p id ="little-tittle-blog"> Retrouvez ici quelques articles sur le développement web.</p>
        
+    <div id="col-tablette1">
     <div className='realisations'>
     <Card style={{ width: '25rem' }}>
-      <Card.Img variant="top" src="/src/assets/blog/art1.png" />
+      <Card.Img variant="top" src="/src/assets/blog/art1.png" className='img-blog' />
       <Card.Body>
         <Card.Title className='blog'>Coder son site en HTML/CSS</Card.Title>
         <Card.Text className='text'> 
@@ -18,7 +19,7 @@ const Blog = () => {
           bulk of the card's content.
         </Card.Text>
         <Button variant="primary">Lire la suite</Button>
-        <Card.Text>
+        <Card.Text className="techno-blog">
           Publié le 22 août 2022
         </Card.Text>
       </Card.Body>
@@ -27,7 +28,7 @@ const Blog = () => {
 
     <div className='realisations'>
     <Card style={{ width: '28rem' }}>
-      <Card.Img variant="top" src="/src/assets/blog/art2.png" />
+      <Card.Img variant="top" src="/src/assets/blog/art2.png" className='img-blog'/>
       <Card.Body>
         <Card.Title className='blog'>Vendre ses produits sur le web</Card.Title>
         <Card.Text>
@@ -35,7 +36,7 @@ const Blog = () => {
           bulk of the card's content.
         </Card.Text>
         <Button variant="primary">Lire la suite</Button>
-        <Card.Text>
+        <Card.Text className="techno-blog">
           Publié le 22 août 2022
         </Card.Text>
       </Card.Body>
@@ -45,7 +46,7 @@ const Blog = () => {
 
     <div className='realisations'>
     <Card style={{ width: '28rem' }}>
-      <Card.Img variant="top" src="/src/assets/blog/art4.png" />
+      <Card.Img variant="top" src="/src/assets/blog/art4.png" className='img-blog'/>
       <Card.Body>
         <Card.Title className='blog'>Coder en responsive design</Card.Title>
         <Card.Text>
@@ -53,16 +54,18 @@ const Blog = () => {
           bulk of the card's content.
         </Card.Text>
         <Button variant="primary">Lire la suite</Button>
-        <Card.Text>
+        <Card.Text className="techno-blog">
           Publié le 22 août 2022
         </Card.Text>
       </Card.Body>
     </Card>
     </div>
+    </div>  
 
     <div className='realisations'>
+    <div id="col-tablette2">
     <Card style={{ width: '28rem' }}>
-      <Card.Img variant="top" src="/src/assets/blog/art5.png" />
+      <Card.Img variant="top" src="/src/assets/blog/art5.png" className='img-blog'/>
       <Card.Body>
         <Card.Title className='blog'>Techniques de référencement</Card.Title>
         <Card.Text>
@@ -70,7 +73,7 @@ const Blog = () => {
           bulk of the card's content.
         </Card.Text>
         <Button variant="primary">Lire la suite</Button>
-        <Card.Text>
+        <Card.Text className="techno-blog">
           Publié le 22 août 2022
         </Card.Text>
       </Card.Body>
@@ -79,7 +82,7 @@ const Blog = () => {
 
     <div className='realisations'>
     <Card style={{ width: '28rem' }}>
-      <Card.Img variant="top" src="/src/assets/blog/art6.png" />
+      <Card.Img variant="top" src="/src/assets/blog/art6.png" className='img-blog'/>
       <Card.Body>
         <Card.Title className='blog'>Apprendre à coder </Card.Title>
         <Card.Text>
@@ -87,7 +90,7 @@ const Blog = () => {
           bulk of the card's content.
         </Card.Text>
         <Button variant="primary">Lire la suite</Button>
-        <Card.Text>
+        <Card.Text className="techno-blog">
           Publié le 22 août 2022
         </Card.Text>
       </Card.Body>
@@ -96,7 +99,7 @@ const Blog = () => {
 
     <div className='realisations'>
     <Card style={{ width: '28rem' }}>
-      <Card.Img variant="top" src="/src/assets/blog/art3.png" />
+      <Card.Img variant="top" src="/src/assets/blog/art3.png" className='img-blog'/>
       <Card.Body>
         <Card.Title className='blog'>Se positionner sur Google</Card.Title>
         <Card.Text>
@@ -104,13 +107,13 @@ const Blog = () => {
           bulk of the card's content.
         </Card.Text>
         <Button variant="primary">Lire la suite</Button>
-        <Card.Text>
+        <Card.Text className="techno-blog">
           Publié le 22 août 2022
         </Card.Text>
       </Card.Body>
     </Card>
     </div>
-    
+    </div>
     </div>
        
        
