480a0d8b33103a76e8abc137db06cd421c8bb15c
N0xCam <167630075+N0xCam@users.noreply.github.com>
2024-11-05 12:33:26 +0100

    home-ok

--- src/pages/home/footer.jsx
+++ src/pages/home/footer.jsx
@@ -68,7 +68,7 @@ return (
           <div className="footer-desktop">
           <div className="footer-tablette-col">
           <div className="liens-realisations">
-            <h3 className="title-realisation">Mes réalisations</h3>
+            <h3 className="title-realisation">Mes dernières réalisations</h3>
             <Link to={"/realisations"}>
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="chevron" viewBox="0 0 16 16">
             <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708"/>
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -9,6 +9,7 @@ function MyHome () {
         <Header/>
         <div className="home">
            
+           <div id="myhome">
            <header id="Introduction">
             <h2 id="Hello"> Bonjour, je suis John Doe</h2>
             <h2 id="Job"> Développeur web full stack</h2>
@@ -17,22 +18,23 @@ function MyHome () {
             </div>
             </header>
 
-            <div id="presentation">
+            <div id="aboutme">
             <div className='colonnes'>
             <section id="presentation">
-                <div id='test'>
+               
                 <h2 id="about"> À propos </h2>
               
                 <article className="mypresentation">
-                <p className='text'>Passionné par l'informatique et les nouvelles technologies, j'ai suivi une formation d' <strong>intégrateur-développeur web </strong>au CEF. Au cours de cette formation, j'ai pu acquérir des bases solides pour travailler dans le domaine du <strong>développement web</strong>. </p>
+                <p className='text'>Passionné par l'informatique et les nouvelles technologies, j'ai suivi une formation d' <strong>intégrateur-développeur web </strong>au CEF. Au cours de cette formation, j'ai pu acquérir des bases solides pour travailler dans le domaine du <strong>développement web.</strong> </p>
 
-                <p className='text'>Basé à Lyon, je suis en recherche d'une alternance au sein d'une agence digitale pour consolider ma formation de <strong>développeur web full stack</strong>.</p>
+                <p className='text'>Basé à Lyon, je suis en recherche d'une alternance au sein d'une agence digitale pour consolider ma formation de <strong>développeur web full stack.</strong></p>
 
                 <p className='text'>J'accorde une attention particulière à la qualité du code que j'écris et je respecte les bonnes pratiques du web.</p>
 
                 </article>
-                </div>
+               
             </section>
+           
             </div>
 
             <div id="second">
@@ -114,7 +116,7 @@ function MyHome () {
                   style={{ width: "50%" }}
                 ></div>
                 </div>
-          
+            </div>
             </div>
             </div>
             </div>
